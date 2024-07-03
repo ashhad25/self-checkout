a1ab63aebd415c09b49883f0a937819bf221c38d
Ashhad Ahmed <81470672+ashhad25@users.noreply.github.com>
2024-07-03 23:58:12 +0500

    UI ideas & application colors

---
+++ UI ideas/app_colors.dart
@@ -0,0 +1,10 @@
+class AppColors{
+	static const Color primaryColor = Color(0xff4CAF50);
+	static const Color lightPrimaryColor = Color(0xffC8E6C9);
+	static const Color darkPrimaryColor = Color(0xff388E3C);
+	static const Color textIconColor = Color(0xffFFFFFF);
+	static const Color dividerColor = Color(0xffBDBDBD);
+	static const Color secondaryTextColor = Color(0xff757575);
+	static const Color primaryTextColor = Color(0xff212121);
+	static const Color accentColor = Color(0xff4CAF50); 
+}
